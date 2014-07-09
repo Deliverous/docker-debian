42f20127eab2224c54e3daf0ef48a9abf7d9995e
Thomas Clavier <tom@tcweb.org>
2014-07-09 11:35:56 +0200

    sleeting this repo

--- wheezy/make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /tmp/
-/usr/sbin/debootstrap wheezy /tmp/wheezy 
-/bin/tar --directory=/tmp/wheezy --create . | /usr/bin/docker import - deliverous/wheezy
-docker push deliverous/wheezy
